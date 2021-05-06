ef8aaf8e1b2e7b2107f9037a4a39ce60fcee6b11
GvitorSilva <83681897+GvitorSilva@users.noreply.github.com>
2021-05-06 00:24:04 -0300

    Add files via upload

---
+++ Calculo de Média.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+nome = input('Insira seu nome completo: ')
+    
+nota1 = float(input('Sua primeira nota: '))
+nota2 = float(input('Sua segunda nota: '))
+
+print('Olá', nome, 'suas notas foram: ')
+print(nota1, 'e', nota2)
+
+soma = nota1 + nota2
+media = soma / 2
+
+print('Sua média é', media)
+    
+
+
+# In[ ]:
+
+
+
+
